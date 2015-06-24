664f5782b4d0d24748217bbef772c38f4efe7760
mahdavipanah <h.mahdavipanah@gmail.com>
2015-06-24 15:03:26 +0430

    Seperate test files

--- test/test.cc
+++ test/test.cc
@@ -3,9 +3,3 @@
 
 #include "../src/stringplus.h"
 using namespace stringplus;
-
-TEST_CASE( "to_upper", "[to_upper]" ) {
-  REQUIRE( to_upper("abcd") == "ABCD" );
-  REQUIRE( to_upper("ABCD") == "ABCD" );
-  REQUIRE( to_upper("1234[") == "1234[" );
-}
---
+++ test/to_upper.cc
@@ -0,0 +1,10 @@
+#include "CATCH/catch.hpp"
+
+#include "../src/stringplus.h"
+using namespace stringplus;
+
+TEST_CASE( "to_upper", "[to_upper]" ) {
+  REQUIRE( to_upper("abcd") == "ABCD" );
+  REQUIRE( to_upper("ABCD") == "ABCD" );
+  REQUIRE( to_upper("1234[") == "1234[" );
+}
